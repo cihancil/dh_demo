ac2216906ba604b124a4da3e6f9bf1a8d6004d7f
Cihan Cil <cihancil@gmail.com>
2024-02-03 22:29:55 +0300

    log fix

--- App.tsx
+++ App.tsx
@@ -2,11 +2,15 @@ import React from 'react'
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
 import { Provider } from 'react-redux'
 import { QueryClientProvider } from 'react-query'
+import { LogBox } from 'react-native'
 
 import Navigator from './src/Navigator'
 import store from './src/store'
 import { queryClient } from './src/query/queryClient'
 import { BottomSheetModalProvider } from '@gorhom/bottom-sheet'
+
+// will be fixed in next RN update  https://github.com/facebook/react-native/issues/42728
+LogBox.ignoreLogs(['RCTBridge required dispatch_sync to load'])
 
 function App(): React.JSX.Element {
   return (
